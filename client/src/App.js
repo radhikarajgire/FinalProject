c7eb759a82105a8ef8f1fb71301583db20d30ce9
feychou <federica.recanatini@gmail.com>
2020-09-25 17:05:19 +0200

    Add drawer to sidebar

--- client/src/App.js
+++ client/src/App.js
@@ -10,33 +10,56 @@ import Register from "./Components/auth/Register";
 import Alert from "./Components/layout/Alert";
 import Dashboard from "./Components/dashboard/Dashboard";
 import PrivateRoute from "./Components/routing/PrivateRoute";
-import { makeStyles } from "@material-ui/core/styles";
-import Drawer from "@material-ui/core/Drawer";
-import CssBaseline from "@material-ui/core/CssBaseline";
-import AppBar from "@material-ui/core/AppBar";
-import Toolbar from "@material-ui/core/Toolbar";
-import List from "@material-ui/core/List";
-import Typography from "@material-ui/core/Typography";
-import Divider from "@material-ui/core/Divider";
-import ListItem from "@material-ui/core/ListItem";
-import ListItemIcon from "@material-ui/core/ListItemIcon";
-import ListItemText from "@material-ui/core/ListItemText";
-import InboxIcon from "@material-ui/icons/MoveToInbox";
-import MailIcon from "@material-ui/icons/Mail";
+import clsx from 'clsx';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+import Drawer from '@material-ui/core/Drawer';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import List from '@material-ui/core/List';
+import Typography from '@material-ui/core/Typography';
+import Divider from '@material-ui/core/Divider';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import MailIcon from '@material-ui/icons/Mail';
 
+// redux
 import { Provider } from "react-redux";
 import store from "./store";
 import { loadUser } from "./actions/auth";
 import setAuthToken from "./utils/setAuthToken";
+
 const drawerWidth = 240;
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    display: "flex",
+    display: 'flex',
   },
   appBar: {
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+  },
+  appBarShift: {
     width: `calc(100% - ${drawerWidth}px)`,
     marginLeft: drawerWidth,
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  hide: {
+    display: 'none',
   },
   drawer: {
     width: drawerWidth,
@@ -45,91 +68,88 @@ const useStyles = makeStyles((theme) => ({
   drawerPaper: {
     width: drawerWidth,
   },
-  // necessary for content to be below app bar
-  toolbar: theme.mixins.toolbar,
+  drawerHeader: {
+    display: 'flex',
+    alignItems: 'center',
+    padding: theme.spacing(0, 1),
+    // necessary for content to be below app bar
+    ...theme.mixins.toolbar,
+    justifyContent: 'flex-end',
+  },
   content: {
     flexGrow: 1,
-    backgroundColor: theme.palette.background.default,
     padding: theme.spacing(3),
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    marginLeft: -drawerWidth,
+  },
+  contentShift: {
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+    marginLeft: 0,
   },
 }));
 
-//Redux
-
-/*import MenuBar from "./Components/menubar/MenuBar.js";
-import Header from "./Components/header/Header.js";
-import FlashCard from "./Components/flashcard/FlashCard.js";
-import Snap from "./Components/snap/Snap.js";
-import WordSearch from "./Components/wordsearch/WordSearch.js";
-import MultiTest from "./Components/multitest/MultiTest.js";
-import Match from "./Components/match/Match.js"
-import SpellShot from "./Components/spellshot/SpellShot.js"
-import StudyPartners from "./Components/studypartners/StudyPartners.js"
-import PoshOrNot from "./Components/poshornot/PoshOrNot.js"
-import ListeningPractice from "./Components/listeningpractice/ListeningPractice.js"
-import FooterTwo from "./Components/footertwo/FooterTwo.js";*/
-//import MDBFooter from "./Components/footer/footer.js";
-
-if (localStorage.token) {
+/*if (localStorage.token) {
   setAuthToken(localStorage.token);
-}
+}*/
 //<img src={logo} className="App-logo" alt="logo" />
 
-const App = () => {
+/*const App = () => {
   useEffect(() => {
     store.dispatch(loadUser());
   }, []);
-};
+};*/
 
 function Appe() {
-  const { menuitem } = useContext(StateContext);
+  const { menuitem, openSidebar, setOpenSidebar } = useContext(StateContext);
   const classes = useStyles();
-  //const [singlerender, setSingleRender]=useState()
 
-  //useEffect(()=>{
-  //const renderarray = {ba: <FlashCard/>, bb: <Snap/>, bc: <WordSearch/>, ca: <ListeningPractice/>,cb: <MultiTest/>, da: <SpellShot/>, dd: <PoshOrNot/>, de: <Match/>, eb: <StudyPartners/>}
-  //const singre = renderarray[menuitem]
-  //setSingleRender(singre)},[menuitem])
+  const theme = useTheme();
+
 
   return (
     <Provider store={store}>
       <Router>
         <Fragment>
           <Navbar />
-          <Drawer
-            className={classes.drawer}
-            variant="permanent"
-            classes={{
-              paper: classes.drawerPaper,
-            }}
-            anchor="left"
-          >
-            <div className={classes.toolbar} />
-            <Divider />
-            <List>
-              {["Inbox", "Starred", "Send email", "Drafts"].map(
-                (text, index) => (
-                  <ListItem button key={text}>
-                    <ListItemIcon>
-                      {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                    </ListItemIcon>
-                    <ListItemText primary={text} />
-                  </ListItem>
-                )
-              )}
-            </List>
-            <Divider />
-            <List>
-              {["All mail", "Trash", "Spam"].map((text, index) => (
-                <ListItem button key={text}>
-                  <ListItemIcon>
-                    {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                  </ListItemIcon>
-                  <ListItemText primary={text} />
-                </ListItem>
-              ))}
-            </List>
-          </Drawer>
+<Drawer
+        className={classes.drawer}
+        variant="persistent"
+        anchor="left"
+        open={openSidebar}
+        classes={{
+          paper: classes.drawerPaper,
+        }}
+      >
+        <div className={classes.drawerHeader}>
+          <IconButton onClick={() => setOpenSidebar(false)}>
+            {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+          </IconButton>
+        </div>
+        <Divider />
+        <List>
+          {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+            <ListItem button key={text}>
+              <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItem>
+          ))}
+        </List>
+        <Divider />
+        <List>
+          {['All mail', 'Trash', 'Spam'].map((text, index) => (
+            <ListItem button key={text}>
+              <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItem>
+          ))}
+        </List>
+      </Drawer>
           <Route exact path="/" component={Landing} />
           <section className="container">
             <Alert />
@@ -144,18 +164,5 @@ function Appe() {
     </Provider>
   );
 }
-
-/* <Header />
-
-      <MenuBar />
-      {singlerender}
-      <FooterTwo />
-
-      <SocialFollow />  
-
-    </div>
-  );
-}
-*/
 
 export default Appe;
--- client/src/Components/statecontext/stateContext.js
+++ client/src/Components/statecontext/stateContext.js
@@ -7,7 +7,7 @@ export default function StateContextProvider({ children }) {
   const [choice, setChoice] = useState('')
   const [playmusic, setPlayMusic] = useState(true)
   const [volsp, setVolsp] = useState(100)
-  
+  const [openSidebar, setOpenSidebar] = useState(false);
 
     const customElements=[{q:"A four leged friend", a: "dog", waone:'chimp', watwo:'parot', wathree:'sheep', wafour:'hedge hog'}, {q:"A moody house pet", a:"cat", waone:'chimp', watwo:'parot', wathree:'sheep', wafour:'hedge hog'}, {q:"Lives in the sea", a:"fish", waone:'chimp', watwo:'parot', wathree:'sheep', wafour:'hedge hog'}, {q:"Worm like", a:"snake", waone:'chimp', watwo:'parot', wathree:'sheep', wafour:'hedge hog'}]
     
@@ -32,7 +32,22 @@ export default function StateContextProvider({ children }) {
 
     return (
         <StateContext.Provider
-          value={{customElements, words, menuitem, setMenuItem, choice, setChoice, playmusic, setPlayMusic, musicchoice, volsp, setVolsp, poshornotchoice, listeningpractice
+          value={{
+            customElements,
+            words,
+            menuitem,
+            setMenuItem,
+            choice,
+            setChoice,
+            playmusic,
+            setPlayMusic,
+            musicchoice,
+            volsp,
+            setVolsp,
+            poshornotchoice,
+            listeningpractice,
+            openSidebar,
+            setOpenSidebar
 
         }}
         >
