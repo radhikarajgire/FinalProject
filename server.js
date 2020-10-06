d6700fa1ab7eb44c8bb7ab2c099e05a01e114ee0
radhikarajgire <58374458+radhikarajgire@users.noreply.github.com>
2020-10-06 15:15:02 +0200

    preparing for deploying

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const connectDB = require("./config/db");
+const path = require('path');
 const app = express();
 const cors = require("cors");
 
@@ -7,7 +8,7 @@ const cors = require("cors");
 connectDB();
 
 // Init Middleware
-app.use(express.json());
+app.use(express.json({ extended: false}));
 app.use(cors());
 
 //define routes
@@ -16,6 +17,8 @@ app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/profile", require("./routes/api/profile"));
 app.use("/api/notes", require("./routes/api/notes"));
 
+
+// Serve static assets in production
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
   app.get("*", (req, res) => {
@@ -25,65 +28,4 @@ if (process.env.NODE_ENV === "production") {
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started  on port ${PORT}`));
-const mongo = require('mongodb').MongoClient;
 
-// // Connect to mongo
-// mongo.connect('mongodb://127.0.0.1/mongochat', function(err, db){
-//     if(err){
-//         throw err;
-//     }
-
-//     console.log('MongoDB connected...');
-
-//     // Connect to Socket.io
-//     client.on('connection', function(socket){
-//         let chat = db.collection('chats');
-
-//         // Create function to send status
-//         sendStatus = function(s){
-//             socket.emit('status', s);
-//         }
-
-//         // Get chats from mongo collection
-//         chat.find().limit(100).sort({_id:1}).toArray(function(err, res){
-//             if(err){
-//                 throw err;
-//             }
-
-//             // Emit the messages
-//             socket.emit('output', res);
-//         });
-
-//         // Handle input events
-//         socket.on('input', function(data){
-//             let name = data.name;
-//             let message = data.message;
-
-//             // Check for name and message
-//             if(name == '' || message == ''){
-//                 // Send error status
-//                 sendStatus('Please enter a name and message');
-//             } else {
-//                 // Insert message
-//                 chat.insert({name: name, message: message}, function(){
-//                     client.emit('output', [data]);
-
-//                     // Send status object
-//                     sendStatus({
-//                         message: 'Message sent',
-//                         clear: true
-//                     });
-//                 });
-//             }
-//         });
-
-//         // Handle clear
-//         socket.on('clear', function(data){
-//             // Remove all chats from collection
-//             chat.remove({}, function(){
-//                 // Emit cleared
-//                 socket.emit('cleared');
-//             });
-//         });
-//     });
-// });
